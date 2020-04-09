1e3e72e05519f4eb4be36ed0f427913de2945b38
Nico Bosshard <nico.bosshard@stud.hslu.ch>
2020-04-09 23:14:15 +0200

    Zufallsgenerator für vertical, horizontal und key

---
+++ Matrix_RNG.py
@@ -0,0 +1,14 @@
+from random import *
+import numpy as np
+import sys
+
+n = 6
+potenz = 2**n
+vsize = randint(1, potenz/2-1)
+hsize = potenz-vsize
+ksize = potenz+1
+np.set_printoptions(threshold=sys.maxsize, suppress=True, linewidth=sys.maxsize)
+print(hsize, "x", vsize)
+print("vertical = np." + repr(np.random.randint(0, 2, vsize)))
+print("horizontal = np." + repr(np.random.randint(0, 2, hsize)))
+print("key = np." + repr(np.random.randint(0, 2, ksize)))
