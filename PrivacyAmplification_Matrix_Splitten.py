9093bb329cd4cfdadd5379846c33e37662d4178f
nicoboss <nico@bosshome.ch>
2021-05-08 23:28:00 +0200

    Current block tracker

--- PrivacyAmplification_Matrix_Splitten.py
+++ PrivacyAmplification_Matrix_Splitten.py
@@ -59,6 +59,19 @@ toeplitz_seed_length = len(toeplitz_seed)
 key = np.array([1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1])
 key_length = horizontal_len
 
+verticalBlocks = 2
+rNr = 0
+for columnNr in range(horizontal_len//8-1, -1, -1):
+    print("columnNr:", columnNr)
+    for keyNr in range(columnNr, columnNr+min((horizontal_len//8-1)-columnNr+1, verticalBlocks), 1):
+        print(keyNr)
+        rNr += 1
+for rowNr in range(1, verticalBlocks, 1):
+    print("rowNr:", rowNr)
+    for keyNr in range(rowNr, min(horizontal_len//8+1, verticalBlocks-rowNr+1), 1):
+        print(keyNr)
+        rNr += 1
+
 local_seed = np.hstack((toeplitz_seed[8:16], toeplitz_seed[0:8])).astype(int)
 print(local_seed)
 local_key_padded = np.hstack((np.zeros(1), key[key_length-8:key_length], np.zeros(7))).astype(int)
