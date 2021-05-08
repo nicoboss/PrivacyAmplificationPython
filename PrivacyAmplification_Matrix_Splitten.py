41a26cfa4adc33c54a01039eade98d5526ae5604
nicoboss <nico@bosshome.ch>
2021-05-08 22:35:01 +0200

    First Block automated

--- PrivacyAmplification_Matrix_Splitten.py
+++ PrivacyAmplification_Matrix_Splitten.py
@@ -59,12 +59,20 @@ toeplitz_seed_length = len(toeplitz_seed)
 key = np.array([1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1])
 key_length = horizontal_len
 
+local_seed = np.hstack((toeplitz_seed[16:24], toeplitz_seed[8:16])).astype(int)
+local_key = key[0:8]
+local_key_padded = np.hstack((np.zeros(1), local_key, np.zeros(7))).astype(int)
+print(local_seed)
+print(local_key_padded)
+amp_key_T1 = permutate(local_seed, local_key_padded)
+
 local_seed = np.hstack((toeplitz_seed[8:16], toeplitz_seed[0:8])).astype(int)
 local_key = key[key_length-8:key_length]
 local_key_padded = np.hstack((np.zeros(1), local_key, np.zeros(7))).astype(int)
 print(local_seed)
 print(local_key_padded)
-amp_key = permutate(local_seed, local_key_padded)
+amp_key_T2 = permutate(local_seed, local_key_padded)
+
 exit(1)
 
 #vertical = np.array([1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0])
