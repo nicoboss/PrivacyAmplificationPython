6eb2c009be398e95a0f80a2ed22ca4006447b64f
nicoboss <nico@bosshome.ch>
2021-05-17 16:31:37 +0200

    Fixed GitLab URL inside the pretty_git_log shell script

--- pretty_git_log.sh
+++ pretty_git_log.sh
@@ -1,1 +1,1 @@
-git log --pretty=format:"https://gitlab.enterpriselab.ch/fm-20fs/cufft/commit/%H %x09%ad%x09%s" --date=format:'%a, %Y-%m-%d' --reverse
+git log --pretty=format:"https://gitlab.enterpriselab.ch/qkd/cpu/privacyamplificationpython/commit/%H %x09%ad%x09%s" --date=format:'%a, %Y-%m-%d' --reverse
